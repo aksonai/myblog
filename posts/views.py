40f0928a0ceeffe071bc0283cc544a5309990fec
fgerra <fatjon.gerra@ikubinfo.al>
2020-12-12 14:03:42 +0100

    added view and template for post_list

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from posts.models import Post
 
-# Create your views here.
+
+def post_list(request):
+    all_posts = Post.objects.all()
+    print('test', all_posts)
+    return render(request, 'posts/post_list.html', {})
