3df64dae6d7a30c04fe9a9b80f43e69bf6be191e
fgerra <fatjon.gerra@ikubinfo.al>
2020-12-19 13:02:23 +0100

    [ADD] view and template for post detail

--- posts/urls.py
+++ posts/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from posts import views
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
+    path('', views.post_list, name='post-list'),
+    path('posts/<int:pk>', views.post_detail, name='post-detail'),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,8 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from posts.models import Post
+from django.utils import timezone
 
 
 def post_list(request):
-    all_posts = Post.objects.all()
-    print('test', all_posts)
-    return render(request, 'posts/post_list.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now())
+    return render(request, 'posts/post_list.html', {'posts': posts})
+
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'posts/post_detail.html', {'post': post})
